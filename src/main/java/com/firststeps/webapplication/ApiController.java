fba0e139e2ab6f11116a492aff26c6c498df1fd6
juanmaMacGyverCode <jmlpdr.93@gmail.com>
2024-08-16 01:06:13 +0100

    new RestController ApiController

---
+++ src/main/java/com/firststeps/webapplication/ApiController.java
@@ -0,0 +1,21 @@
+package com.firststeps.webapplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ApiController {
+    private final VideoService videoService;
+
+    public ApiController(VideoService videoService) {
+        this.videoService = videoService;
+    }
+
+    @GetMapping("/api/videos")
+    public List<Video> all() {
+        return videoService.getVideos();
+    }
+
+}
